1994c7be17f57f29c5e26d68fd03c769a1a0a2b5
shumpei.sugita <shumpei.sugita@fj.ics.keio.ac.jp>
2024-07-31 20:05:22 +0900

    consider key frame to bpy rendering

--- develop/step2-batch-eye-rendering/code/scene_renderer.py
+++ develop/step2-batch-eye-rendering/code/scene_renderer.py
@@ -1,6 +1,7 @@
 import bpy
 import os
 from os.path import abspath
+import time
 import math
 from mathutils import Vector, Quaternion, Euler
 class SceneRenderer:
@@ -54,11 +55,11 @@ class SceneRenderer:
             return False
         self.scene_path = scene_path
         return True 
-    def render_image(self, camera, width = 100, height = 100,samples = 10,base_file_name = 'image'):
-        # レンダリング設定
+        
+    def setup_rendering_common(self, width, height, samples):
         bpy.context.scene.render.engine                     = 'CYCLES'
         bpy.context.scene.render.image_settings.file_format = 'PNG'
-        #bpy.context.scene.render.image_settings.use_preview = False
+        # py.context.scene.render.image_settings.use_preview = False
         bpy.context.scene.render.filepath                   = self.output_path + '/image.png'
         bpy.context.scene.render.resolution_x               = width
         bpy.context.scene.render.resolution_y               = height
@@ -67,6 +68,40 @@ class SceneRenderer:
         bpy.context.scene.cycles.device                     = 'GPU'
         bpy.context.scene.use_nodes = True
         bpy.context.scene.view_layers["ViewLayer"].use_pass_z = True
+
+        cycles_preferences = bpy.context.preferences.addons['cycles'].preferences
+        print(str(cycles_preferences))
+
+        selected_device_type = None
+        
+        for device in cycles_preferences.devices:
+            print ("supported device.name is "+device.type)
+#            if device.type == 'OPTIX':
+#                selected_device_type = device.type
+#                break
+            if device.type == 'CUDA':
+                selected_device_type = device.type
+        
+        if selected_device_type is None:
+            selected_device_type = cycles_preferences.devices[0]
+        # 環境マップをロード
+        image = bpy.data.images.load(self.background_image_path)
+        if selected_device_type is None:
+            selected_device_type = cycles_preferences.devices[0].type
+        # # デバイスタイプをGPUに設定
+        cycles_preferences.compute_device_type = selected_device_type
+        print ("selected device.name is "+selected_device_type)
+        # # シーンの設定
+        scene = bpy.context.scene
+        world = scene.world
+        node_background = world.node_tree.nodes['Background']
+        # Add Environment Texture node
+        node_environment = world.node_tree.nodes.new('ShaderNodeTexEnvironment')
+        node_environment.image = image
+        # Link all nodes
+        links = world.node_tree.links
+        link  = links.new(node_environment.outputs["Color"], node_background.inputs["Color"])
+    def setup_rendering_frame (self, base_file_name_per_frame):
         scene_node_tree = bpy.context.scene.node_tree
         # clear default nodes
         for node in scene_node_tree.nodes:
@@ -100,13 +135,13 @@ class SceneRenderer:
         node_save_color.base_path =  self.output_color_path
         node_save_color.file_slots.clear()  # 既存のスロットをクリア
         node_save_color.file_slots.new('MyOutputImage')  # 新しいスロットを追加
-        node_save_color.file_slots[0].path  = base_file_name + '_' 
+        node_save_color.file_slots[0].path  = base_file_name_per_frame + '_' 
         node_save_depth = scene_node_tree.nodes.new('CompositorNodeOutputFile')
         node_save_depth.name = 'save_depth'
         node_save_depth.base_path =  self.output_depth_path
         node_save_depth.file_slots.clear()  # 既存のスロットをクリア
         node_save_depth.file_slots.new('MyOutputImage')  # 新しいスロットを追加
-        node_save_depth.file_slots[0].path = base_file_name + '_'
+        node_save_depth.file_slots[0].path = base_file_name_per_frame + '_'
         if self.output_depth_format == 'OPEN_EXR':
             node_save_depth.format.file_format = "OPEN_EXR" # default is "PNG"
             node_save_depth.format.color_mode  = "RGB"  # default is "BW"
@@ -118,45 +153,8 @@ class SceneRenderer:
             node_save_depth.format.color_depth = "16"
             node_save_depth.format.compression = 0
 
-
         scene_node_tree.links.new(output_color,node_save_color.inputs[0])
         scene_node_tree.links.new(output_depth,node_save_depth.inputs[0])
-
-        cycles_preferences = bpy.context.preferences.addons['cycles'].preferences
-        # デバイスタイプをGPUに設定
-        cycles_preferences.compute_device_type = 'CUDA'
-
-        # CUDAデバイスタイプを選択している場合は、OptiXを有効にする
-        if cycles_preferences.compute_device_type == 'CUDA':
-            cycles_preferences.get_devices()
-
-            for device in cycles_preferences.devices:
-                if device.type == 'CUDA' and device.name == 'NVIDIA CUDA':
-                    print("FOUND: CUDA")
-                    device.use = True
-                    cycles_preferences.compute_device = device.name
-                    cycles_preferences.compute_device_type = 'OPTIX'
-                    break
-
-
-        # 環境マップをロード
-        image = bpy.data.images.load(self.background_image_path)
-
-        # シーンの設定
-        scene = bpy.context.scene
-        world = scene.world
-        node_background = world.node_tree.nodes['Background']
-
-        # Add Environment Texture node
-        node_environment = world.node_tree.nodes.new('ShaderNodeTexEnvironment')
-        node_environment.image = image
-        # Link all nodes
-        links = world.node_tree.links
-        link  = links.new(node_environment.outputs["Color"], node_background.inputs["Color"])
-        # カメラの設定
-        scene.camera = camera
-        # レンダリング実行
-        bpy.ops.render.render(write_still=True)
 
     def rotate_camera(self, camera, base_location,base_radius, base_rotation,phi, theta):
         # base_locationは, カメラの位置を球面上に配置するための中心座標
@@ -175,38 +173,96 @@ class SceneRenderer:
         # 回転軸はlocal_x_axis
         # 回転角はtheta
         rotation_matrix = Quaternion(local_x_axis, -theta).to_matrix().to_4x4() @ rotation_matrix
-
         # カメラの姿勢として設定
         camera.matrix_world =  camera.matrix_world @ rotation_matrix
-        camera.location     = camera_location
-
-    def run(self):
-        width = 100
-        height = 100
-        samples = 10
-        base_file_name = 'image'
-        # カメラを格納する変数を初期化
-        base_camera = None
+        camera.location     = base_location
+        
+    def find_base_camera(self):
         # シーン内のすべてのオブジェクトを取得
-        objects = bpy.context.scene.objects
+        objects     = bpy.context.scene.objects
         # すべてのオブジェクトをチェックしてカメラを見つける
         for obj in objects:
             if obj.type == 'CAMERA':
                 base_camera = obj
-
         # カメラが見つかった場合は、それを基準とする
         if base_camera:
             print("Camera found:", base_camera.name)
         else:
             print("No camera found in the scene.")
-        
-        # 個眼間角度を取得(度数)→ラジアンに変換して視野角へ
-        ommatidium_angle = self.input_settings['ommatidium_angle']
-        camera_field_of_view = ommatidium_angle * 3.141592653589793 / 180.0
+        return base_camera
+
+    def render_frame_image(self,base_filename, base_camera, frame_index, field_of_view, radius):
+        # シーンを指定されたキーフレームへ移動
+        bpy.context.scene.frame_set(frame_index)
         # ベースカメラの位置を取得
         camera_location = base_camera.location
         # ベースカメラの姿勢を取得
         camera_rotation = base_camera.rotation_euler
+        cameras = []
+        i = 0
+        for center in self.input_settings['centers']:
+            # 見つかったカメラを複製する
+            camera      = base_camera.copy()
+            camera.data = base_camera.data.copy()
+            # animationをクリア
+            camera.animation_data_clear()
+            camera.name = base_camera.name + '_center_' + str(i) + '_frame_' + str(frame_index)
+            # 複製したカメラオブジェクトを現在のシーンにリンク
+            camera.data.type           = 'PERSP'
+            camera.data.clip_start     = base_camera.data.clip_start
+            camera.data.clip_end       = base_camera.data.clip_end
+            camera.data.angle          = field_of_view
+            # 中心角を取得(phiは0~360, thetaは0~180)
+            (center_phi, center_theta) = center
+            # ラジアンへ変換
+            center_phi   = center_phi   * 3.141592653589793 / 180.0
+            center_theta = center_theta * 3.141592653589793 / 180.0
+            self.rotate_camera (camera, camera_location, radius, camera_rotation, center_phi, center_theta)  
+            # 
+            bpy.context.collection.objects.link(camera)
+            cameras.append(camera)
+            # 時間計測開始
+            beg = time.time()
+            # 出力ファイル名は, image_${i}.png
+            base_file_name_per_frame = base_filename + '_' +  str(i).zfill(4)
+            # レンダリング設定のセットアップ
+            self.setup_rendering_frame(base_file_name_per_frame)
+            bpy.context.scene.camera = camera
+            # レンダリング実行
+            bpy.ops.render.render(write_still=True)
+            # 時間計測終了
+            end = time.time()
+            # 経過時間を表示(秒)
+            print("Time:", end - beg)
+            i = i+1
+
+    def render_images(self,base_filename,base_camera, frame_start, frame_end, field_of_view, radius):
+        # フレーム番号を開始から終了まで繰り返す
+        for frame_index in range(frame_start, frame_end + 1):
+            # フレーム番号を設定
+            bpy.context.scene.frame_set(frame_index)
+            # フレーム番号を表示
+            print("Frame:", frame_index)
+            # レンダリング実行
+            self.render_frame_image(base_filename, base_camera,frame_index,field_of_view, radius)
+
+        
+    def run(self):
+        width   = 100
+        height  = 100
+        samples = 10
+        # レンダリング設定のセットアップ(共通)
+        self.setup_rendering_common(width, height, samples)
+        # 開始フレーム, 終了フレームを取得
+        frame_start      = bpy.context.scene.frame_start
+        frame_end        = bpy.context.scene.frame_end
+        # DEBUG: 処理負荷軽減のためにフレーム数を1に制限
+        frame_end        = min(frame_end, frame_start )
+        base_file_name   = 'image'
+        # カメラを格納する変数を初期化
+        base_camera      = self.find_base_camera()
+        # 個眼間角度を取得(度数)→ラジアンに変換して視野角へ
+        field_of_view    = self.input_settings['ommatidium_angle'] * 3.141592653589793 / 180.0
         # カメラのクリッピング範囲を指定
         if self.input_settings is None:
             print ("input_settings is None")
@@ -215,33 +271,8 @@ class SceneRenderer:
             print ("centers not found in input_settings")
             return
         radius = 1.0
-        num_centers = len(self.input_settings['centers'])
-        i = 0
-        for center in self.input_settings['centers']:
-            # 見つかったカメラを複製する
-            camera = base_camera.copy()
-            camera.data = base_camera.data.copy()
-            camera.name = base_camera.name + '_center_' + str(i)
-            # 複製したカメラオブジェクトを現在のシーンにリンク
-            bpy.context.collection.objects.link(camera)
-            camera.data.clip_start = 0.001
-            camera.data.clip_end   = 10000
-            camera.data.angle = camera_field_of_view
-            # 中心角を取得(phiは0~360, thetaは0~180)
-            (center_phi, center_theta) = center
-            # ラジアンへ変換
-            center_phi = center_phi * 3.141592653589793 / 180.0
-            center_theta = center_theta * 3.141592653589793 / 180.0
-            self.rotate_camera (camera, camera_location, radius, camera_rotation, center_phi, center_theta)
-            # 出力ファイル名は, image_${i}.png
-            # iは0から始まり, 4桁になるように0埋め
-            filename = base_file_name + '_' +  str(i).zfill(4)
-            # レンダリング実行
-            self.render_image(camera, width, height, samples, filename)
-            i = i+1
-
-        # # 複製したカメラを削除する
-        # bpy.data.objects.remove(camera)
+        # 描画処理を実行
+        self.render_images(base_file_name, base_camera,frame_start,frame_end,field_of_view, radius)
 
     def print(self):
         print("scene_path=",self.scene_path)
@@ -251,7 +282,7 @@ class SceneRenderer:
 
 if __name__ == "__main__":
     input_settings = {
-    'centers': [(-6.0, -3.375), (-4.5, -3.375), (-3.0, -3.375), (-1.5, -3.375), (0.0, -3.375), (1.5, -3.375), (3.0, -3.375), (4.5, -3.375), (-5.25, -2.08125), (-3.75, -2.08125), (-2.25, -2.08125), (-0.75, -2.08125), (0.75, -2.08125), (2.25, -2.08125), (3.75, -2.08125), (5.25, -2.08125), (-6.0, -0.78125), (-4.5, -0.78125), (-3.0, -0.78125), (-1.5, -0.78125), (0.0, -0.78125), (1.5, -0.78125), (3.0, -0.78125), (4.5, -0.78125), (-5.25, 0.51875), (-3.75, 0.51875), (-2.25, 0.51875), (-0.75, 0.51875), (0.75, 0.51875), (2.25, 0.51875), (3.75, 0.51875), (5.25, 0.51875), (-6.0, 1.81875), (-4.5, 1.81875), (-3.0, 1.81875), (-1.5, 1.81875), (0.0, 1.81875), (1.5, 1.81875), (3.0, 1.81875), (4.5, 1.81875), (-5.25, 3.11875), (-3.75, 3.11875), (-2.25, 3.11875), (-0.75, 3.11875), (0.75, 3.11875), (2.25, 3.11875), (3.75, 3.11875), (5.25, 3.11875)],
+    'centers': [(-6.0, -3.375), (-4.5, -3.375), (-3.0, -3.375), (-1.5, -3.375), (0.0, -3.375), (1.5, -3.375), (3.0, -3.375), (4.5, -3.375), (-5.25, -2.08125), (-3.75, -2.08125), (-2.25, -2.08125), (-0.75, -2.08125), (0.75, -2.08125), (2.25, -2.08125), (3.75, -2.08125), (5.25, -2.08125), (-6.0, -0.78125), (-4.5, -0.78125), (-3.0, -0.78125), (-1.5, -0.78125), (0.0, -0.78125), (1.5, -0.78125), (3.0, -0.78125), (4.5, -0.78125), (-5.25, 0.51875), (-3.75, 0.51875), (-2.25, 0.51875), (-0.75, 0.51875), (0.75, 0.51875), (2.25, 0.51875), (3.75, 0.51875), (5.25, 0.51875), (-6.0, 1.81875), (-4.5, 1.81875), (-3.0, 1.81875), (-1.5, 1.81875), (0.0, 1.81875), (1.5, 1.81875), (3.0, 1.81875), (4.5, 1.81875), (-5.25, 3.11875), (-3.75, 3.11875), (-2.25, 3.11875), (-0.75, 3.11875), (0.75, 3.11875), (2.25, 3.11875), (3.75, 3.11875), (5.25, 3.11875)],      
     'theta': 0,
     'phi': 0,
     'ommatidium_angle': 1.5,
@@ -260,3 +291,4 @@ if __name__ == "__main__":
     renderer = SceneRenderer(output_depth_format='OPEN_EXR', input_settings=input_settings)
     renderer.print()
     renderer.run()
+    
