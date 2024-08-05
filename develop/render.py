69b0e7b81678236c3f944e3ce380b27a3c9811b2
shumpei.sugita <shumpei.sugita@fj.ics.keio.ac.jp>
2024-08-05 16:13:59 +0900

    add animation loop

--- develop/render.py
+++ develop/render.py
@@ -3,6 +3,26 @@ from step1_calculate_pixel_viewport.code import calculate_pixel_viewport
 from step2_batch_eye_rendering.code      import scene_renderer
 from step3_apply_hexigonal_filter.code   import apply_hexigonal_filter
 
+def rander_frame(settings):
+    basedir = os.path.dirname(os.path.abspath(__file__))
+    color_image_dir = os.path.join(basedir,"output","temp_color_image")
+    depth_image_dir = os.path.join(basedir,"output","temp_depth_image")
+    output_dir = os.path.join(basedir,"output")
+    print(color_image_dir)
+    print(output_dir)
+
+    hex_pos_setting = calculate_pixel_viewport.run(settings)
+    scene_path = settings["scene_path"]
+    frame_index = settings["frame"]
+    scene_renderer.run(scene_path, hex_pos_setting, color_image_dir,depth_image_dir,frame_index) # Generate Images in output_color/output_depth folder
+    apply_hexigonal_filter.run(settings,color_image_dir, output_dir,frame_index) # Generate Images in output_color/output_depth folder
+
+def render_animation(settings):
+    frames = [1]
+    for frame in frames:
+        settings["frame"] = frame
+        rander_frame(settings)
+
 settings = {
     "scene_path"           : "D:/Users/shums/kasika/20240703/compound-eye-simulator/assets/flower_middle_compress.blend", 
     "output_width"         : 1920,
@@ -14,23 +34,11 @@ settings = {
     "ommatidium_radius"    : 0.00001,
     "theta"                : 0,
     "phi"                  : 0,
-    "filter"               : "hexagonal_depth_gaussian",
-    "view_mode"            : "depth",
-    "debug_mode"           : False,
+    "filter"               : "hexagonal_gaussian",
+    "view_mode"            : "color",
+    "debug_mode"           : True,
     "blur_size"            : 60,
     "frame"                : 1
 }
 
-basedir = os.path.dirname(os.path.abspath(__file__))
-color_image_dir = os.path.join(basedir,"output","temp_color_image")
-depth_image_dir = os.path.join(basedir,"output","temp_depth_image")
-output_dir = os.path.join(basedir,"output")
-print(color_image_dir)
-print(output_dir)
-
-hex_pos_setting = calculate_pixel_viewport.run(settings)
-scene_path = settings["scene_path"]
-frame_index = settings["frame"]
-scene_renderer.run(scene_path, hex_pos_setting, color_image_dir,depth_image_dir,frame_index) # Generate Images in output_color/output_depth folder
-apply_hexigonal_filter.run(settings,color_image_dir, output_dir,frame_index) # Generate Images in output_color/output_depth folder
-
+render_animation(settings)
