5f1b75068c17e75740f93fba476f8aa5800f6ab7
yaikeda <yaikeda@kashika.co.jp>
2024-08-29 20:25:10 +0900

    add 日本語 path support to save images

--- panorama_render/step2-equirectangular-clipping/code/ui_viewer.py
+++ panorama_render/step2-equirectangular-clipping/code/ui_viewer.py
@@ -365,8 +365,24 @@ class UIViewer:
         if not file_path:
             print("保存がキャンセルされました。")
             return
-        cv2.imwrite(file_path, self.view_image)
-        print("現在の視点画像を保存しました: ", file_path.split('/')[-1])
+        self.imwrite(file_path, self.view_image)
+        print("現在の視点画像を保存しました: ", file_path)
+
+    def imwrite(self, path, img): # cv2は日本語パスに対応しないため特殊対応
+        tmp_dir = os.getcwd()
+        # 1. 保存するディレクトリに移動
+        if len(path.split("/")) > 1:
+            file_dir = "/".join(path.split("/")[:-1])
+            os.chdir(file_dir)
+        # 2. 対象ファイルを保存
+        tmp_name = "tmp_name.png"
+        cv2.imwrite(tmp_name, img)
+        # 3. 対象ファイルの名前を戻す
+        if os.path.exists(path.split("/")[-1]):  # ファイルが既にあれば削除
+            os.remove(path.split("/")[-1])
+        os.rename(tmp_name, path.split("/")[-1])
+        # カレントディレクトリをもとに戻す
+        os.chdir(tmp_dir)
 
     def on_save_settings_click(self):
         self.save_settings()
