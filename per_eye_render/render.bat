8a210db01c56780a5f2801d6c8261a8585ecf533
yaikeda <yaikeda@kashika.co.jp>
2024-08-29 20:39:10 +0900

    update setting files.

--- per_eye_render/render.bat
+++ per_eye_render/render.bat
@@ -1,3 +1,4 @@
-set PYTHON_EXE=%~dp0..\Python311\python.exe
+REM set PYTHON_EXE=%~dp0..\Python311\python.exe
+set PYTHON_EXE=python
 %PYTHON_EXE% %~dp0..\per_eye_render\render.py %*
 pause
---
+++ ui_viewer.bat
@@ -0,0 +1,4 @@
+REM set PYTHON_EXE=%~dp0..\..\Python311\python.exe
+set PYTHON_EXE=python
+%PYTHON_EXE% %~dp0\panorama_render\step2-equirectangular-clipping\code\ui_viewer.py --setting settings\settings.toml
+pause
